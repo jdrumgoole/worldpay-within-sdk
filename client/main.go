16b0efe4b0479db07e04681f765a6fda6f60fa5e
Conor Hackett <conor@conorhackett.com>
2016-05-04 14:06:11 +0100

    Add client directory. An area to call the SDK from.

---
+++ client/main.go
@@ -0,0 +1,24 @@
+package client
+import
+(
+	log "github.com/Sirupsen/logrus"
+	"os"
+)
+
+func Main() {
+
+	initLog()
+}
+
+func initLog() {
+
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stderr instead of stdout, could also be a file.
+	log.SetOutput(os.Stderr)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+
+	log.Debug("Log is initialised.")
+}
