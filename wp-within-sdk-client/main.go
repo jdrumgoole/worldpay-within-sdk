75f5d4fc56d021c38bf87993b0424b6017c4ae61
Kevin Gordon <kevin.gordon@worldpay.com>
2016-05-04 16:34:15 +0100

    KG - got a PoC of logging to sockets or browser working

---
+++ wp-within-sdk-client/main.go
@@ -0,0 +1,58 @@
+package main
+import
+(
+	"fmt"
+	"strconv"
+	log "github.com/Sirupsen/logrus"
+	"os"
+	"time"
+	"runtime"
+	"sync"
+)
+
+func main() {
+
+	//initLog()
+
+	testWebSocketServer()
+}
+
+func initLog() {
+
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stderr instead of stdout, could also be a file.
+	log.SetOutput(os.Stderr)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+
+	log.Debug("Log is initialised.")
+}
+
+func testWebSocketServer() {
+	log.Println("testWebSocketServer", nil)
+
+	runtime.GOMAXPROCS(2)
+	var wg sync.WaitGroup
+    wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		entryPoint()
+	}()
+
+	go func() {
+		defer wg.Done()
+		var i = 0
+		for {
+			time.Sleep(time.Duration(2 * time.Second))
+			log.Debug("Should be outputting")
+			EchoLogMsg("i = " + strconv.Itoa(i))
+			i++
+		}
+	}()	
+
+	fmt.Println("Waiting To Finish")
+    wg.Wait()
+}
