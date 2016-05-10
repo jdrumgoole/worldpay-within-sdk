28455c012c0a3695152be3a3d745a3a6021d36bd
Kevin Gordon <kevin.gordon@worldpay.com>
2016-05-10 05:39:53 +0100

    Refactored to have all the webserver hidden in the logging function

--- client/initialise/main.go
+++ client/initialise/main.go
@@ -4,17 +4,14 @@ import
 	"strconv"
 	"os"
 	"time"
-	"runtime"
-	"sync"
 	log "github.com/Sirupsen/logrus"
-	wsserver "innovation.worldpay.com/worldpay-within-sdk/client/Wsserver"
 )
-
-var wssKEV wsserver.Wsserver
 
 func Main() {
 
 	initLog()
+
+	doStuff() // Replace with actually doing stuff!
 
 }
 
@@ -31,42 +28,20 @@ func initLog() {
 
 	log.Debug("Log is initialised.")
 
-	setupServer();
-
-	log.SetSecureSocketsServer(wssKEV)
+	log.SetSecureSocketsServer(true)
 
 }
 
-func setupServer() {
-	var _wssKEV = &wsserver.Wsserver{};
-	wssKEV = *_wssKEV
-	startWebSocketServer()
-}
 
-func startWebSocketServer() {
 
-	runtime.GOMAXPROCS(2)
-	var wg sync.WaitGroup
-    wg.Add(2)
 
-	go func() {
+func doStuff() {
 
-		defer wg.Done()
-		log.Info(wssKEV.ShowSocketClosedMsg());		
-		wssKEV.EntryPoint() 
-
-	}()
-
-	go func() {
-		defer wg.Done()
-		var i = 0
-		for {
-			time.Sleep(time.Duration(2 * time.Second))
-			log.Info("[" + strconv.Itoa(i) + "]")
-			i++
-		}
-	}()	
-
-	wg.Wait()
+	var i = 0
+	for {
+		time.Sleep(time.Duration(2 * time.Second))
+		log.Info("[" + strconv.Itoa(i) + "]")
+		i++
+	}
 
 }
