7c489e6f07d306d42b2ce4319cc16195c44d7321
Conor Hackett <conor@conorhackett.com>
2016-06-03 12:17:51 +0100

    Remove main from sdk core. not needed.

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-import initialise "innovation.worldpay.com/worldpay-within-sdk/client/initialise"
-
-func main() {
-
-	initialise.Main()
-
-
-} // Needed to run the target from within IntelliJ Idea.
