f293d88169583eb1c00a63f6de1fd5045ac20fd1
Conor Hackett <conor@conorhackett.com>
2016-06-03 15:50:08 +0100

    Initial commit of Thrift RPC definitions. First pass is for C# only to get A. Brodie up and running.

---
+++ rpc/wptypes.thrift
@@ -0,0 +1,88 @@
+##############################################
+#
+# Worldpay Within SDK Thrift definition
+# Conor Hackett (conor.hackett@worldpay.com)
+# June 3rd, 2016
+#
+#############################################
+
+namespace csharp wptypes
+
+exception Error {
+	
+	1: string message
+}
+
+struct Service {
+	
+	1: i32 id
+	2: string name
+	3: optional string description
+	4: optional map<i32, Price> prices
+}
+
+struct Price {
+	
+	1: i32 serviceId
+	2: i32 id
+	3: string description
+	4: i32 pricePerUnit
+	5: i32 unitId
+	6: string unitDescription
+}
+
+struct HCECard {
+	
+	1: string FirstName
+	2: string LastName
+	3: i32 ExpMonth
+	4: i32 ExpYear
+	5: string CardNumber
+	6: string Type
+	7: optional string Cvc
+}
+
+struct Device {
+	
+	1: string uid
+	2: string name
+	3: optional string description
+	4: optional map<i32, Service> services
+	5: string ipv4Address
+	6: string currencyCode
+}
+
+struct ServiceMessage {
+	
+	1: string deviceDescription
+	2: string hostname
+	3: i32 portNumber
+	4: string serverId
+	5: string urlPrefix
+}
+
+struct ServiceDetails {
+	
+	1: i32 serviceId
+	2: string serviceDescription
+}
+
+struct TotalPriceResponse {
+	
+	1: string serverId
+	2: string clientId
+	3: i32 priceId
+	4: i32 unitsToSupply
+	5: i32 totalPrice
+	6: string paymentReferenceId
+	7: string merchantClientKey
+}
+
+struct PaymentResponse {
+	
+	1: string serverId
+	2: string clientId
+	3: i32 totalPaid
+	4: string serviceDeliveryToken
+	5: string ClientUUID
+}
---
+++ rpc/wpwithin.thrift
@@ -0,0 +1,40 @@
+##############################################
+#
+# Worldpay Within SDK Thrift definition
+# Conor Hackett (conor.hackett@worldpay.com)
+# June 3rd, 2016
+#
+#############################################
+
+include "wptypes.thrift"
+
+namespace csharp wpwithin
+
+/**
+ * WorldpayWithin Service - exposing all WorldpayWithin SDK functionality
+ */
+service WPWithin {
+
+  /**
+   * A method definition looks like C code. It has a return type, arguments,
+   * and optionally a list of exceptions that it may throw. Note that argument
+   * lists and exception lists are specified using the exact same syntax as
+   * field lists in struct or exception definitions.
+   */
+
+   void addService(1: wptypes.Service svc) throws (1: wptypes.Error err),
+   void removeService(1: wptypes.Service svc) throws (1: wptypes.Error err),
+   void initHCE(1: wptypes.HCECard hceCard) throws (1: wptypes.Error err),
+   void initHTE(1: string merchantClientKey, 2: string merchantServiceKey) throws (1: wptypes.Error err),
+   void initConsumer(1: string scheme, 2: string hostname, 3: i32 port, 4: string urlPrefix, 5: string serviceId) throws (1: wptypes.Error err),
+   void initProducer() throws (1: wptypes.Error err),
+   wptypes.Device getDevice(),
+   void startServiceBroadcast(1: i32 timeoutMillis) throws (1: wptypes.Error err),
+   void stopServiceBroadcast() throws (1: wptypes.Error err),
+   set<wptypes.ServiceMessage> serviceDiscovery(1: i32 timeoutMillis) throws (1: wptypes.Error err),
+   set<wptypes.ServiceDetails> requestServices() throws (1: wptypes.Error err),
+   set<wptypes.Price> getServicePrices(1: i32 serviceId) throws (1: wptypes.Error err),
+   wptypes.TotalPriceResponse selectService(1: i32 serviceId, 2: i32 numberOfUnits, 3: i32 priceId) throws (1: wptypes.Error err),
+   wptypes.PaymentResponse makePayment(1: wptypes.TotalPriceResponse request) throws (1: wptypes.Error err),
+
+}
