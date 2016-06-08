57139f89a5b8e8263edbc83501302dab6c2fc797
Conor Hackett <conor@conorhackett.com>
2016-06-08 22:56:31 +0100

    Remove print statements.

--- sdkcore/wpwithin/types/service.go
+++ sdkcore/wpwithin/types/service.go
@@ -1,5 +1,4 @@
 package types
-import "fmt"
 
 type Service struct {
 
@@ -20,16 +19,12 @@ func NewService() (*Service, error) {
 
 func (service *Service) AddPrice(price Price) error {
 
-	fmt.Printf("Add price. Price UID = %s\n", price.ID)
-
 	service.prices[price.ID] = price
 
 	return nil
 }
 
 func (service *Service) RemovePrice(price Price) error {
-
-	fmt.Println("Remove price..")
 
 	delete(service.prices, price.ID)
 
