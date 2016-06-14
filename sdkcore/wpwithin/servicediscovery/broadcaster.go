fd68b24f45b5eb86646c63bb3ff37a304df6e957
Conor Hackett <conor@conorhackett.com>
2016-06-14 12:18:20 +0100

    - Broadcaster implementation does not run broadcast in goroutine anymore. - Broadcaster interface no longer returns a bool channel to signal when broadcast is finished (for now, anyway) - WPWithin implementation now handles go routines for service broadcast and HTE service instead of calling code.

--- sdkcore/wpwithin/servicediscovery/broadcaster.go
+++ sdkcore/wpwithin/servicediscovery/broadcaster.go
@@ -3,6 +3,6 @@ import "innovation.worldpay.com/worldpay-within-sdk/sdkcore/wpwithin/types"
 
 type Broadcaster interface {
 
-	StartBroadcast(msg types.ServiceMessage, timeoutMillis int) (chan bool, error)
+	StartBroadcast(msg types.ServiceMessage, timeoutMillis int) error
 	StopBroadcast() error
 }
