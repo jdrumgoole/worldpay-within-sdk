ce4c1492d085280662305d008f2bc3c3b12c0ff6
Conor Hackett <conor@conorhackett.com>
2016-07-01 16:41:19 +0100

    Fixed formatting after crap copy/paste job.

--- sdkcore/wpwithin_test/hte_test/ordermanager_test.go
+++ sdkcore/wpwithin_test/hte_test/ordermanager_test.go
@@ -1,88 +1,88 @@
 package hte_test
 
 import (
-"testing"
-"github.com/stretchr/testify/assert"
-"innovation.worldpay.com/worldpay-within-sdk/sdkcore/wpwithin/types"
+	"github.com/stretchr/testify/assert"
 	"innovation.worldpay.com/worldpay-within-sdk/sdkcore/wpwithin/hte"
+	"innovation.worldpay.com/worldpay-within-sdk/sdkcore/wpwithin/types"
+	"testing"
 )
 
 func TestAddOrder(t *testing.T) {
 
-if om, err := hte.NewOrderManager(); err == nil {
+	if om, err := hte.NewOrderManager(); err == nil {
 
-tmpOrder := types.Order{
-ServiceID: 1,
-ClientID: "client_id_123",
-SelectedNumberOfUnits: 2,
-SelectedPriceId: 3,
-TotalPrice: 199,
-PaymentReference: "pay-ref",
-ClientUUID: "client-uuid-here",
-PSPReference: "pay-ref",
-DeliveryToken: "delivery-token-here",
-}
+		tmpOrder := types.Order{
+			ServiceID:             1,
+			ClientID:              "client_id_123",
+			SelectedNumberOfUnits: 2,
+			SelectedPriceId:       3,
+			TotalPrice:            199,
+			PaymentReference:      "pay-ref",
+			ClientUUID:            "client-uuid-here",
+			PSPReference:          "pay-ref",
+			DeliveryToken:         "delivery-token-here",
+		}
 
-if err := om.AddOrder(tmpOrder); err == nil {
+		if err := om.AddOrder(tmpOrder); err == nil {
 
-if !om.OrderExists(tmpOrder.PaymentReference) {
+			if !om.OrderExists(tmpOrder.PaymentReference) {
 
-assert.Fail(t, "Order does not exist in order manager")
-}
+				assert.Fail(t, "Order does not exist in order manager")
+			}
 
-} else {
+		} else {
 
-assert.Fail(t, "Error adding order to order manager", err.Error())
-}
+			assert.Fail(t, "Error adding order to order manager", err.Error())
+		}
 
-} else {
+	} else {
 
-assert.Fail(t, "Error creating order manager", err.Error())
-}
+		assert.Fail(t, "Error creating order manager", err.Error())
+	}
 }
 
 func TestGetOrder(t *testing.T) {
 
-if om, err := hte.NewOrderManager(); err == nil {
+	if om, err := hte.NewOrderManager(); err == nil {
 
-tmpOrder := types.Order{
-ServiceID: 1,
-ClientID: "client_id_123",
-SelectedNumberOfUnits: 2,
-SelectedPriceId: 3,
-TotalPrice: 199,
-PaymentReference: "pay-ref",
-ClientUUID: "client-uuid-here",
-PSPReference: "pay-ref",
-DeliveryToken: "delivery-token-here",
-}
+		tmpOrder := types.Order{
+			ServiceID:             1,
+			ClientID:              "client_id_123",
+			SelectedNumberOfUnits: 2,
+			SelectedPriceId:       3,
+			TotalPrice:            199,
+			PaymentReference:      "pay-ref",
+			ClientUUID:            "client-uuid-here",
+			PSPReference:          "pay-ref",
+			DeliveryToken:         "delivery-token-here",
+		}
 
-err := om.AddOrder(tmpOrder)
+		err := om.AddOrder(tmpOrder)
 
-if err != nil {
+		if err != nil {
 
-assert.Fail(t, "Error adding order ", err.Error())
-}
+			assert.Fail(t, "Error adding order ", err.Error())
+		}
 
-if order, err := om.GetOrder(tmpOrder.PaymentReference); err == nil {
+		if order, err := om.GetOrder(tmpOrder.PaymentReference); err == nil {
 
-assert.Equal(t, 1, order.ServiceID)
-assert.Equal(t, "client_id_123", order.ClientID)
-assert.Equal(t, 2, order.SelectedNumberOfUnits)
-assert.Equal(t, 3, order.SelectedPriceId)
-assert.Equal(t, 199, order.TotalPrice)
-assert.Equal(t, "pay-ref", order.PaymentReference)
-assert.Equal(t, "client-uuid-here", order.ClientUUID)
-assert.Equal(t, "pay-ref", order.PSPReference)
-assert.Equal(t, "delivery-token-here", order.DeliveryToken)
+			assert.Equal(t, 1, order.ServiceID)
+			assert.Equal(t, "client_id_123", order.ClientID)
+			assert.Equal(t, 2, order.SelectedNumberOfUnits)
+			assert.Equal(t, 3, order.SelectedPriceId)
+			assert.Equal(t, 199, order.TotalPrice)
+			assert.Equal(t, "pay-ref", order.PaymentReference)
+			assert.Equal(t, "client-uuid-here", order.ClientUUID)
+			assert.Equal(t, "pay-ref", order.PSPReference)
+			assert.Equal(t, "delivery-token-here", order.DeliveryToken)
 
-} else {
+		} else {
 
-assert.Fail(t, "Order does not exist in order manager", err.Error())
-}
+			assert.Fail(t, "Order does not exist in order manager", err.Error())
+		}
 
-} else {
+	} else {
 
-assert.Fail(t, "Error creating order manager", err.Error())
-}
+		assert.Fail(t, "Error creating order manager", err.Error())
+	}
 }
