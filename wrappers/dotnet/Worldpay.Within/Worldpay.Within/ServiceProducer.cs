cf9859607a1295dbda447e73dc39029697143b13
Andy Brodie <andy@locima.co.uk>
2016-07-12 20:18:44 +0100

    More skeleton code

---
+++ wrappers/dotnet/Worldpay.Within/Worldpay.Within/ServiceProducer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worldpay.Innovation.WPWithin
+{
+    /**
+     * Use this if you want to provide a Host Terminal Emulator (HTE).  Being an HTE means that you are providing a service
+     * to consumers that they can pay for.
+     * 
+     * In order to be an HTE you have broadcast the services that you provide to consumers.
+     * 
+     * 
+     *
+     */
+    public class ServiceProducer
+    {
+    }
+}
---
+++ wrappers/dotnet/Worldpay.Within/Worldpay.Within/WPWithinService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worldpay.Innovation.WPWithin
+{
+    class WPWithinService
+    {
+        /*
+   void addService(1: wptypes.Service svc) throws (1: wptypes.Error err),
+   void removeService(1: wptypes.Service svc) throws (1: wptypes.Error err),
+   void initHCE(1: wptypes.HCECard hceCard) throws (1: wptypes.Error err),
+   void initHTE(1: string merchantClientKey, 2: string merchantServiceKey) throws (1: wptypes.Error err),
+   void initConsumer(1: string scheme, 2: string hostname, 3: i32 port, 4: string urlPrefix, 5: string serviceId) throws (1: wptypes.Error err),
+   void initProducer() throws (1: wptypes.Error err),
+   wptypes.Device getDevice(),
+   void startServiceBroadcast(1: i32 timeoutMillis) throws (1: wptypes.Error err),
+   void stopServiceBroadcast() throws (1: wptypes.Error err),
+   set<wptypes.ServiceMessage> serviceDiscovery(1: i32 timeoutMillis) throws (1: wptypes.Error err),
+   set<wptypes.ServiceDetails> requestServices() throws (1: wptypes.Error err),
+   set<wptypes.Price> getServicePrices(1: i32 serviceId) throws (1: wptypes.Error err),
+   wptypes.TotalPriceResponse selectService(1: i32 serviceId, 2: i32 numberOfUnits, 3: i32 priceId) throws (1: wptypes.Error err),
+   wptypes.PaymentResponse makePayment(1: wptypes.TotalPriceResponse request) throws (1: wptypes.Error err),
+*/
+
+
+    }
+}
