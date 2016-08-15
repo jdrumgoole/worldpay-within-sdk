14f59e7aba559225f259e3af6dcef5d01acdd518
Conor Hackett <conor@conorhackett.com>
2016-08-15 18:09:50 +0100

    Clean up converter.

--- wrappers/nodejs/library/wpwithin.js
+++ wrappers/nodejs/library/wpwithin.js
@@ -47,7 +47,9 @@ var fnAddService = function(service, callback) {
 
 var fnRemoveService = function(service, callback) {
 
-  this.thriftClient.removeService(service, function(err, result) {
+  var convSvc = this.converter.toThrift().service(service);
+
+  this.thriftClient.removeService(convSvc, function(err, result) {
 
     callback(err, result);
   });
@@ -129,7 +131,9 @@ var fnSelectService = function(serviceId, numberOfUnits, priceId, callback) {
 
 var fnMakePayment = function(request, callback) {
 
-  this.thriftClient.makePayment(request, function(err, result) {
+  var convRequest = this.converter.toThrift().totalPriceResponse(request);
+
+  this.thriftClient.makePayment(convRequest, function(err, result) {
 
     callback(err, result);
   });
@@ -150,22 +154,6 @@ var fnEndServiceDelivery = function(clientId, serviceDeliveryToken, unitsReceive
     callback(err, result);
   });
 };
-
-// void setup(1: string name, 2: string description) throws (1: wptypes.Error err),
-// void addService(1: wptypes.Service svc) throws (1: wptypes.Error err),
-// void removeService(1: wptypes.Service svc) throws (1: wptypes.Error err),
-// void initConsumer(1: string scheme, 2: string hostname, 3: i32 port, 4: string urlPrefix, 5: string serverId, 6: wptypes.HCECard hceCard) throws (1: wptypes.Error err),
-// void initProducer(1: string merchantClientKey, 2: string merchantServiceKey) throws (1: wptypes.Error err),
-// wptypes.Device getDevice(),
-// void startServiceBroadcast(1: i32 timeoutMillis) throws (1: wptypes.Error err),
-// void stopServiceBroadcast() throws (1: wptypes.Error err),
-// set<wptypes.ServiceMessage> deviceDiscovery(1: i32 timeoutMillis) throws (1: wptypes.Error err),
-// set<wptypes.ServiceDetails> requestServices() throws (1: wptypes.Error err),
-// set<wptypes.Price> getServicePrices(1: i32 serviceId) throws (1: wptypes.Error err),
-// wptypes.TotalPriceResponse selectService(1: i32 serviceId, 2: i32 numberOfUnits, 3: i32 priceId) throws (1: wptypes.Error err),
-// wptypes.PaymentResponse makePayment(1: wptypes.TotalPriceResponse request) throws (1: wptypes.Error err),
-// void beginServiceDelivery(1: string clientId, 2: wptypes.ServiceDeliveryToken serviceDeliveryToken, 3: i32 unitsToSupply) throws (1: wptypes.Error err),
-// void endServiceDelivery(1: string clientId, 2: wptypes.ServiceDeliveryToken serviceDeliveryToken, 3: i32 unitsReceived) throws (1: wptypes.Error err),
 
 // Factory setup WPWithinClient
 // Should return an instance of WPWithin
