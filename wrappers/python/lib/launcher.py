be33b52c65361f0b00272c2958f4090b3ef455f7
Salih Oztop <soztop@yahoo.com>
2016-08-31 13:06:52 +0100

    arm architecture added to Launcher

--- wrappers/python/lib/launcher.py
+++ wrappers/python/lib/launcher.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 ## Python Launcher
-## Sample ./launcher.py /home "{'linux-32':'rpc-agent-linux-32','linux-64':'rpc-agent-linux-64','win-32':'rpc-agent-win-32.exe','win-64':'rpc-agent-win-64.exe'}"
 from subprocess import call
 import sys
 import ast
@@ -8,7 +7,9 @@ import platform
 
 def os_arch():
     """Give the architecture as 32 or 64"""
-    if sys.maxsize > 2**32:
+    if platform.machine().lower()[:3] == 'arm':
+        out = 'arm'
+    elif sys.maxsize > 2**32:
         out = '64'
     else:
         out = '32'
