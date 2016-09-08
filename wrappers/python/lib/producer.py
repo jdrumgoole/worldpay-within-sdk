5c7a2876b358388360fa0c8d3c00263b0896eaf3
Mariana Araujo <marianabdaraujo@gmail.com>
2016-09-08 14:10:21 +0100

    service prices should be dict

--- wrappers/python/lib/producer.py
+++ wrappers/python/lib/producer.py
@@ -11,7 +11,7 @@ client.setup("Python3 Device", "Sample Python3 producer device")
 
 pricePerUnit = types.PricePerUnit(amount=650, currencyCode="GBP")
 rwPrice = types.Price(id=1, description="Car Wash", pricePerUnit=pricePerUnit, unitId=1, unitDescription="Single wash")
-service = types.Service(id=1, name="RoboWash", description="Car washed by robot", prices=[rwPrice])
+service = types.Service(id=1, name="RoboWash", description="Car washed by robot", prices={1: rwPrice})
 
 client.addService(service)
 
